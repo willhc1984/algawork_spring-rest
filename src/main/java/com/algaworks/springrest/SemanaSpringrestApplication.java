e2526b115daab5efc298e789b6bf2143b48ab111
will henrique <willhc1984@gmail.com>
2022-09-14 13:00:49 -0300

    Inicio de Projeto - IDE STS

---
+++ src/main/java/com/algaworks/springrest/SemanaSpringrestApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaSpringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaSpringrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/springrest/SemanaSpringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaSpringrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
