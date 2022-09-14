0d4918c60b631f4e59aebc2527a1625e1167a10e
will henrique <willhc1984@gmail.com>
2022-09-14 15:15:12 -0300

    BeanValidation - Cliente

--- src/main/java/com/algaworks/springrest/controllers/ClienteController.java
+++ src/main/java/com/algaworks/springrest/controllers/ClienteController.java
@@ -2,6 +2,8 @@ package com.algaworks.springrest.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,12 +48,12 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody Cliente cliente){
+	public Cliente salvar(@Valid @RequestBody Cliente cliente){
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @Valid @RequestBody Cliente cliente){
 		if(!clienteRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/com/algaworks/springrest/model/Cliente.java
+++ src/main/java/com/algaworks/springrest/model/Cliente.java
@@ -8,6 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_cliente")
@@ -18,8 +21,17 @@ public class Cliente implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 100)
 	private String nome;
+	
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 	public Cliente() {
