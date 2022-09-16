9566b28cbe94b8784f9d9cdf125626335a33e75b
will henrique <willhc1984@gmail.com>
2022-09-16 14:49:36 -0300

    Implement. ModelMapper - Entregas

---
+++ src/main/java/com/algaworks/springrest/common/EntregaAssembler.java
@@ -0,0 +1,37 @@
+package com.algaworks.springrest.common;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.springrest.model.Entrega;
+import com.algaworks.springrest.model.DTO.EntregaDTO;
+
+@Component
+public class EntregaAssembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public EntregaDTO toDTO(Entrega entrega) {
+		return modelMapper.map(entrega, EntregaDTO.class);
+	}
+
+	public EntregaAssembler(ModelMapper modelMapper) {
+		super();
+		this.modelMapper = modelMapper;
+	}
+	
+	public List<EntregaDTO> toCollectionDTO(List<Entrega> entregas){
+		return entregas.stream()
+				.map(this::toDTO)
+				.collect(Collectors.toList());
+	}
+	
+	
+	
+}
--- src/main/java/com/algaworks/springrest/controllers/EntregaController.java
+++ src/main/java/com/algaworks/springrest/controllers/EntregaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.springrest.common.EntregaAssembler;
 import com.algaworks.springrest.model.Entrega;
 import com.algaworks.springrest.model.DTO.DestinatarioDTO;
 import com.algaworks.springrest.model.DTO.EntregaDTO;
@@ -31,35 +33,44 @@ public class EntregaController {
 	@Autowired
 	private EntregaRepository entregaRepository;
 	
+	@Autowired
+	private EntregaAssembler entregaAssembler;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaDTO solicitar(@Valid @RequestBody Entrega entrega) {
+		return entregaAssembler.toDTO(solicitacaoEntregaService.solicitar(entrega));
 	}
 	
 	@GetMapping
-	public List<Entrega> listar(){
-		return entregaRepository.findAll();
+	public List<EntregaDTO> listar(){
+		return entregaAssembler.toCollectionDTO(entregaRepository.findAll());
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<EntregaDTO> buscar(@PathVariable Long id) {
 		return entregaRepository.findById(id)
 				.map(entrega -> {
-					EntregaDTO entregaDTO = new EntregaDTO();
-					entregaDTO.setId(entrega.getId());
-					entregaDTO.setNomeCliente(entrega.getCliente().getNome());
-					entregaDTO.setDestinatario(new DestinatarioDTO());
-					entregaDTO.getDestinatario().setNome(entrega.getDestinatario().getNome());
-					entregaDTO.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-					entregaDTO.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-					entregaDTO.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-					entregaDTO.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-					entregaDTO.setTaxa(entrega.getTaxa());
-					entregaDTO.setStatus(entrega.getStatus());
-					entregaDTO.setDataFinalizacao(entrega.getDataFinalizacao());
-					entregaDTO.setDataPedido(entrega.getDataPedido());
+					EntregaDTO entregaDTO = entregaAssembler.toDTO(entrega);
 					return ResponseEntity.ok(entregaDTO);
+					/*
+					 * Codigo tradicional - BoilerPlate
+					 * EntregaDTO entregaDTO = new EntregaDTO(); entregaDTO.setId(entrega.getId());
+					 * entregaDTO.setNomeCliente(entrega.getCliente().getNome());
+					 * entregaDTO.setDestinatario(new DestinatarioDTO());
+					 * entregaDTO.getDestinatario().setNome(entrega.getDestinatario().getNome());
+					 * entregaDTO.getDestinatario().setLogradouro(entrega.getDestinatario().
+					 * getLogradouro());
+					 * entregaDTO.getDestinatario().setBairro(entrega.getDestinatario().getBairro())
+					 * ; entregaDTO.getDestinatario().setComplemento(entrega.getDestinatario().
+					 * getComplemento());
+					 * entregaDTO.getDestinatario().setNumero(entrega.getDestinatario().getNumero())
+					 * ; entregaDTO.setTaxa(entrega.getTaxa());
+					 * entregaDTO.setStatus(entrega.getStatus());
+					 * entregaDTO.setDataFinalizacao(entrega.getDataFinalizacao());
+					 * entregaDTO.setDataPedido(entrega.getDataPedido()); return
+					 * ResponseEntity.ok(entregaDTO);
+					 */
 				})
 				.orElse(ResponseEntity.notFound().build());
 	}
