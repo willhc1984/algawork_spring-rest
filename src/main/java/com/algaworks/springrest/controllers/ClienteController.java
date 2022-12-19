0b51c23ec631da66564a6eafe7c95cbc3736d0af
will henrique <willhc1984@gmail.com>
2022-12-19 11:00:36 -0300

    Ajustes no projeto

--- src/main/java/com/algaworks/springrest/controllers/ClienteController.java
+++ src/main/java/com/algaworks/springrest/controllers/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.algaworks.springrest.model.Cliente;
 import com.algaworks.springrest.repositories.ClienteRepository;
 import com.algaworks.springrest.services.ClienteService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
